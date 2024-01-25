e7e551c8486579b0bd07184e9ab30a9cb39c49b1
tsu-kuyomi <hezekiahbanks@gmail.com>
2024-01-25 09:45:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const video = document.getElementById("myVideo");
+
+// Get the button
+const btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+// For Nav BAr
